3b03b3a73546ef12fd4b006e280bcc2ea30f003a
vladimir greenfield <vladdygreenfield@gmail.com>
2021-11-12 15:58:18 -0500

    Created a NameList component, finished excercise 2

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,19 +1,19 @@
 
 import React, {Component} from 'react';
 import DisplayName from './DisplayName/DisplayName';
-
+import NameList from './NamesList/NameList';
 
 class App extends Component {
     state = {
-        firstName: 'Reggie',
-        lastName: 'White'
-    };
+        names: ['Mike', 'Nevin', 'Aaron', 'Tory', 'Kellie']
+    }
 
     render() {
         return (
            <div className="App">
                <h1>React Component Worksheet 1.</h1>
                <DisplayName firstName={this.state.firstName} lastName={this.state.lastName} />
+               <NameList />
            </div>
         );
     }
---
+++ src/Components/NamesList/NameList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const NameList = (props) => {
+    return (
+        <div className="nameList">
+            <ul>
+                <li>Mike</li>
+                <li>Nevin</li>
+                <li>Aaron</li>
+                <li>Tory</li>
+                <li>Kellie</li>
+            </ul>
+        </div>
+    );
+}
+
+export default NameList;
